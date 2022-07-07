0f27a82a89ac2319fcbac30a7384243ac4c0aa36
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-07-07 17:40:55 +0200

    navbar in JSX

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-ReactDOM.render(<h1 className = "header">Hello React</h1> , document.getElementById("root"))
+const navbar = (
+    <nav>
+        <h1>Brand Name</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
 
-
-
-
-//const h1 = document.createElement("h1")
-
-//h1.textContent = "This is the imperative way"
-//h1.className = "header"
-//document.getElementById('root').append(h1)
+ReactDOM.render(navbar, document.getElementById("root"))
