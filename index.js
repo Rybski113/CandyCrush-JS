f732c5d2842ac5d7ed04b6a82eead8bee75d6c22
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-07-07 17:04:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-ReactDOM.render(<h1>Hello, React</h1>, document.getElementById("root"))
+ReactDOM.render(
+    <p>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
+    </p>
+    , document.getElementById("root"))
