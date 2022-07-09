99879d76da2c033055fbd97b9e8a1f97f1e29677
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-07-09 14:09:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-
-
-
-
-
-
-function TemporaryName() {
+function ReactList() {
     return (
-
-        <div>
-       <h1>Fun facts about React</h1>
-       <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K start on Github</li>
-        <li>Is maintained by Facebook</li>
-        <li>Power thousands of entrprise apps, including mobile apps</li>
-       </ul>
-    </div>
+        <ul>
+            <li>React is great</li>
+            <li>React is great</li>
+            <li>React is great</li>
+            <li>React is great</li>
+            <li>React is great</li>
+        </ul>
     )
 }
 
-ReactDOM.render(<TemporaryName/>, document.getElementById("root"))
+ReactDOM.render(<ReactList />, document.getElementById("root"))
