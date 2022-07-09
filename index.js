9e8f5172c447001f273ef44a6ddc824f7d7cdfc8
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-07-09 14:34:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 function Page() {
     return(
-        <div>
-            <header>
+        
+        <div>   
+            <Header />
+            <MainContent/>
+           <Footer />
+        </div>
+        
+    )
+}
+
+function Header() {
+    return(
+        
+        <header>
             <nav>
                 <h1>React Library</h1>
             </nav>
         </header>
+    )
+}
+
+function Footer() {
+  return(
+    <footer>
+        <small>2022 Rybski113 development. All right reserved</small>
+    </footer> 
+  )
+       
+}
+
+function MainContent() {
+    return (
         <main>
             <h3>React gives great opportunities</h3>
         </main>
-        <footer>
-             <small>2022 Rybski113 development. All right reserved</small>
-        </footer>
-        </div>
-        
     )
 }
 
